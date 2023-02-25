99518b8c7f126c00218bb5018bb8b76bec452f6d
Itsvenomo <120070832+Itsvenomo@users.noreply.github.com>
2023-02-25 16:27:55 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,14 +2,14 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("🌀 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🌀", callback_data="generate")]
 
     generate_button = [generate_single_button]
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/Its_venom_family"),
-         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/Venom_bolte_public"),
+        [InlineKeyboardButton("😈 sᴜᴩᴩᴏʀᴛ 😈", url="https://t.me/Bothub_xD"),
+         InlineKeyboardButton("❄️ ᴅᴇᴠᴇʟᴏᴩᴇʀ ❄️", url="https://t.me/Finex_xd"),
         ],
     ]
 
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/ItzvenomXd/StringGenBot)
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ᴍʀ ᴀʏᴜsʜ](https://t.me/Venom_bolte_public) !
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://t.me/Bothub_xD)
+Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ғɪɴᴇx](https://t.me/Finex_xd) !
     """
